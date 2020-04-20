231188dfc7972a21f9b1e56978329e767adf199e
codehecker <codehecker@gmail.com>
2020-04-20 06:50:22 +0530

    Upload All files

---
+++ Scripts/firebase.init.js
@@ -0,0 +1,13 @@
+﻿// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBmFUky0-vgmpmnoU0bBBLgALpQ28pPQVc",
+    authDomain: "seabottle.firebaseapp.com",
+    databaseURL: "https://seabottle.firebaseio.com",
+    projectId: "seabottle",
+    storageBucket: "seabottle.appspot.com",
+    messagingSenderId: "984524470086",
+    appId: "1:984524470086:web:f71cc759d8e443fb18d747",
+    measurementId: "G-JC175ZXTGV"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
